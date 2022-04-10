2292e7f80f16ae0d782c60d1907d3d4d28b9f440
kidussintayehu <kidussintayehu60@gmail.com>
2022-04-10 18:19:14 +0300

    column,row and stack

---
+++ lib/resipe_list_screen.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/material.dart';
+// void main() {
+//   runApp( RecipeListScreen());
+// }
+
+class RecipeListScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Recipe"),
+      ),
+     
+    );
+  }
+}
