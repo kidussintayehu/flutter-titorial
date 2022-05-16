9574eec9b5c851debf549419f391619e4527dc52
kidussintayehu <kidussintayehu60@gmail.com>
2022-05-16 14:56:39 +0300

    before ading stack of circles

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,67 @@
-import 'package:firstapp/scaffold_props.dart';
 import 'package:flutter/material.dart';
 
+void main() => runApp(const MyApp());
 
-void main() {
-  runApp(const RecipeApp());
-}
-
-class RecipeApp extends StatelessWidget {
-  const RecipeApp({Key? key}) : super(key: key);
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final color = [
+      Colors.indigo,
+      Colors.black,
+      Colors.green,
+      Colors.lightBlue,
+      Colors.indigo,
+      Colors.black,
+      Colors.green,
+      Colors.lightBlue,
+      Colors.indigo,
+      Colors.black,
+      Colors.green,
+      Colors.lightBlue,
+      Colors.indigo,
+      Colors.black,
+      Colors.green,
+      Colors.lightBlue,
+      Colors.indigo,
+      Colors.black,
+      Colors.green,
+      Colors.lightBlue
+    ];
+    const title = 'Horizontal List';
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(title),
+        ),
+        body: Container(
+          height: 500,
+          width: double.infinity,
+          color: const Color.fromARGB(255, 105, 213, 237),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                for (int i = 0; i < 20; i++)
+                Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Container(
+                    width: 200,
+                    height: 200,
+                    padding: const EdgeInsets.only(left: 4.0, right: 4.0),
+                    color: color[i],
+                    // child: Container(),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
-      home: ScaffoldPropsScreen(),
     );
   }
 }
